93895f60c2f0621188b701d5480d8b40202ef811
Dai Akatsuka <d.akatsuka@gmail.com>
2026-05-23 23:23:02 +0900

    test: cover direct request path matching

--- test/test_request.ml
+++ test/test_request.ml
@@ -10,6 +10,17 @@ let test_path_strips_query () =
 let test_root_path () =
   check string "path" "/" (Camelio.Request.path (request "/"))
 
+let test_path_can_be_split_for_direct_matching () =
+  match
+    Camelio.Request.path (request "/users/42?tab=profile")
+    |> String.split_on_char '/'
+  with
+  | [ ""; "users"; id ] -> check string "id" "42" id
+  | segments ->
+      fail
+        (Printf.sprintf "unexpected path segments: [%s]"
+           (String.concat "; " segments))
+
 let test_invalid_target () =
   check_raises "invalid target" (Invalid_argument "invalid origin-form target")
     (fun () -> ignore (request "https://example.test/" : Camelio.Request.t))
@@ -25,6 +36,8 @@ let () =
         [
           test_case "path strips query" `Quick test_path_strips_query;
           test_case "root path" `Quick test_root_path;
+          test_case "path can be split for direct matching" `Quick
+            test_path_can_be_split_for_direct_matching;
           test_case "invalid target" `Quick test_invalid_target;
           test_case "space in target" `Quick test_reject_space_in_target;
         ] );
